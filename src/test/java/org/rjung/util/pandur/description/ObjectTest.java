f8a86a5ccbf620f22d07858caea3323f9448adff
Rainer Jung <Rainer.Jung@gmail.com>
2017-02-12 14:27:53 +0100

    cleanup

--- src/test/java/org/rjung/util/pandur/description/ObjectTest.java
+++ src/test/java/org/rjung/util/pandur/description/ObjectTest.java
@@ -1,5 +1,8 @@
 package org.rjung.util.pandur.description;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.junit.Assert.assertThat;
+
 import java.beans.IntrospectionException;
 
 import org.junit.Test;
@@ -8,7 +11,9 @@ import org.rjung.util.pandur.beans.User;
 public class ObjectTest {
 
   @Test
-  public void bla() throws IntrospectionException {
-    new Object<>(User.class);
+  public void PropertiesAreExtractedFromGivenClass() throws IntrospectionException {
+    final Object<User> object = new Object<>(User.class);
+
+    assertThat(object.getProperties(), hasItems("id", "email", "passwordEncrypted"));
   }
 }
